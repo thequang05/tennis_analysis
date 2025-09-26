c23e0c12ea506bbfced0eb9c1f9e80168483e9fa
23001549@hus.edu.vn <23001549@hus.edu.vn>
2025-09-26 15:48:42 +0700

    Update bbox_utils

--- tennis_analysis_project/utils/bbox_utils.py
+++ tennis_analysis_project/utils/bbox_utils.py
@@ -8,7 +8,7 @@ def get_width_of_bbox(bbox):
     return bbox[2]-bbox[0]
 def get_height_of_bbox(bbox):
     return bbox[3]-bbox[1]
-def get_area_off_bbox(bbox):
+def get_area_of_bbox(bbox):
     return get_width_of_bbox(bbox)*get_height_of_bbox(bbox)
 def calculate_iou(bbox1,bbox2):
     x_left = max(bbox1[0], bbox2[0])
@@ -28,4 +28,28 @@ def draw_bbox_on_frame(frame,bbox,color=(0,255,0),thickness=2):
     x1,y1,x2,y2=bbox
     cv2.rectangle(frame,(x1,y1),(x2,y2),color,thickness)
     return frame
+
+
+def get_closest_keypoint_index(point, keypoints, keypoint_indices):
+    closest_distance = float('inf')
+    key_point_ind = keypoint_indices[0]
+    for keypoint_indix in keypoint_indices:
+        keypoint = keypoints[keypoint_indix * 2], keypoints[keypoint_indix * 2 + 1]
+        distance = abs(point[1] - keypoint[1])
+
+        if distance < closest_distance:
+            closest_distance = distance
+            key_point_ind = keypoint_indix
+
+    return key_point_ind
+def get_center_of_bbox(bbox):
+    x1, y1, x2, y2 = bbox
+    center_x = int((x1 + x2) / 2)
+    center_y = int((y1 + y2) / 2)
+    return (center_x, center_y)
+def measure_distance(p1,p2):
+    return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)**0.5
+def get_foot_position(bbox):
+    x1, y1, x2, y2 = bbox
+    return (int((x1 + x2) / 2), y2)
 
